76bd93d83da093f5bd9e281094d1ddab0194708f
bvellek <bvellek@uw.edu>
2017-04-11 13:14:11 -0700

    initial user schema

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const mogoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+mongoose.Promise = global.Promise;
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true
+  },
+  firstName: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    trim: true
+  },
+  google: {
+    id: String,
+    token: String
+  },
+
+})
+
+
+userSchema.methods.validPassword = (password) => bcrypt.compareSync(password, this.password);
+
+userSchema.methods.hashPassword = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+
+const User = mongoose.model('User', userSchema);
+
+
+module.exports = User;
