f7fb0ec5e73d5cf43b6a2cea6adbe733706c774a
bvellek <bvellek@uw.edu>
2017-04-14 01:27:17 -0700

    create Footer component

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../../App.css';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        <Footer />
       </div>
     );
   }
---
+++ client/src/js/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="landing-footer" role="contentinfo">Designed and Developed by <a href="https://benvellek.com/" target="_ben-portfolio">BEN VELLEK</a></footer>
+);
+
+export default Footer;
