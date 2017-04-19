c4a56e8af2285d7409d5f2798c481a994312a911
bvellek <bvellek@uw.edu>
2017-04-18 17:31:07 -0700

    add login and register action tests

--- client/src/__test__/actions.test.js
+++ client/src/__test__/actions.test.js
@@ -10,3 +10,73 @@ describe('Loading Status Action', () => {
     expect(actions.loadingStatusTrue()).toEqual(expectedAction);
   });
 });
+
+// Register User Test
+describe('Register User Success Action', () => {
+  it('should create an action to show register user success and pass the registrationSuccessMessage', () => {
+    const testMsg = {
+      message: 'Register Succesful',
+      status: 'Success',
+    };
+    const expectedAction = {
+      type: 'REGISTER_USER_SUCCESS',
+      registrationSuccessMessage: {
+        message: 'Register Succesful',
+        status: 'Success',
+      },
+    };
+    expect(actions.registerUserSuccess(testMsg)).toEqual(expectedAction);
+  });
+});
+
+describe('Register User Error Action', () => {
+  it('should create an action to show register user error and pass the registrationErrorMessage', () => {
+    const testMsg = {
+      message: 'Passwords do not match',
+      status: 'Fail',
+    };
+    const expectedAction = {
+      type: 'REGISTER_USER_ERROR',
+      registrationErrorMessage: {
+        message: 'Passwords do not match',
+        status: 'Fail',
+      },
+    };
+    expect(actions.registerUserError(testMsg)).toEqual(expectedAction);
+  });
+});
+
+// Login User Test
+describe('Login User Success Action', () => {
+  it('should create an action to show login user success and pass the loginSuccessMessage', () => {
+    const testMsg = {
+      message: 'Login Successful',
+      status: 'Success',
+    };
+    const expectedAction = {
+      type: 'LOGIN_USER_SUCCESS',
+      loginSuccessMessage: {
+        message: 'Login Successful',
+        status: 'Success',
+      },
+    };
+    expect(actions.loginUserSuccess(testMsg)).toEqual(expectedAction);
+  });
+});
+
+describe('Login User Error Action', () => {
+  it('should create an action to show login user error and pass the loginErrorMessage', () => {
+    const testMsg = {
+      message: 'Email not found',
+      status: 'Fail',
+    };
+    const expectedAction = {
+      type: 'LOGIN_USER_ERROR',
+      loginErrorMessage: {
+        message: 'Email not found',
+        status: 'Fail',
+      },
+    };
+    expect(actions.loginUserError(testMsg)).toEqual(expectedAction);
+  });
+});
