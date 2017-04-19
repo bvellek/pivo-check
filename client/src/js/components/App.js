05f4a6a47b1d9bac93df4ab886904bc60bd25b5b
bvellek <bvellek@uw.edu>
2017-04-19 16:41:38 -0700

    refactor routes to check if user is authenticated and redirect

--- client/src/js/components/App.js
+++ client/src/js/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import '../../App.css';
 
+import Auth from './modules/Auth';
 import ScrollToTop from './ScrollToTop';
 import LandingPage from './LandingPage';
 import LoginPageContainer from './LoginPageContainer';
@@ -15,11 +16,36 @@ class App extends Component {
       <BrowserRouter basename='/'>
         <ScrollToTop>
           <div className="App">
-            <Route exact path="/" component={LandingPage} />
-            <Route path="/login" component={LoginPageContainer} />
-            <Route path="/registration" component={RegistrationPageContainer} />
-            <Route exact path="/cities" component={CitiesPage} />
-            <Route path="/cities/:city" component={CityPage} />
+            <Route exact path="/" render={() => ( // eslint-disable-line
+                !Auth.isUserAuthenticated()
+                ? <LandingPage />
+                : <Redirect to="/cities" />
+              )}
+            />
+            <Route path="/login" render={() => ( // eslint-disable-line
+                !Auth.isUserAuthenticated()
+                ? <LoginPageContainer />
+                : <Redirect to="/cities" />
+              )}
+            />
+            <Route path="/registration" render={() => ( // eslint-disable-line
+                !Auth.isUserAuthenticated()
+                ? <RegistrationPageContainer />
+                : <Redirect to="/cities" />
+              )}
+            />
+            <Route exact path="/cities" render={() => ( // eslint-disable-line
+                Auth.isUserAuthenticated()
+                ? <CitiesPage />
+                : <Redirect to="/login" />
+              )}
+            />
+            <Route path="/cities/:city" render={() => ( // eslint-disable-line
+                Auth.isUserAuthenticated()
+                ? <CityPage />
+                : <Redirect to="/login" />
+              )}
+            />
           </div>
         </ScrollToTop>
       </BrowserRouter>
@@ -28,3 +54,9 @@ class App extends Component {
 }
 
 export default App;
+
+// <Route exact path="/" component={LandingPage} />
+// <Route path="/login" component={LoginPageContainer} />
+// <Route path="/registration" component={RegistrationPageContainer} />
+// <Route exact path="/cities" component={CitiesPage} />
+// <Route path="/cities/:city" component={CityPage} />
