1652ed4931db305dd7c481109d193b98d1769d28
bvellek <bvellek@uw.edu>
2017-04-22 21:33:50 -0700

    created city item with Link to cityID

---
+++ client/src/js/components/CitiesItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SVGDelete from './SVGDelete';
+
+const CitiesItem = ({
+  cityInfo,
+}) => (
+  <div>
+    <div className="city-item">
+      <Link to={`/cities/${cityInfo._id}`}><h2 className="city-list-title">{cityInfo.cityName}</h2></Link>
+      <div className="city-info">
+        <p className="city-list-brew-count">{cityInfo.completedCount}/{cityInfo.brewTotal}</p>
+        <form className="city-list-delete-form" action="index.html" method="post">
+          <button type="submit" name="button"><SVGDelete /><span className="visually-hidden">Delete {cityInfo.cityName}</span></button>
+        </form>
+      </div>
+    </div>
+
+    <div className="delete-city-warning">
+      <h3>Delete {cityInfo.cityName}?</h3>
+      <p>Deleting a location will permanently remove it from your checklist and you will lose your visited brewery record.</p>
+      <form className="delete-city-form" action="index.html" method="post">
+        <button className="keep-city-btn" type="button" name="button">No, Keep {cityInfo.cityName}</button> {' '}
+        <button className="delete-city-btn" type="button" name="button"><SVGDelete />&nbsp;Yes, Delete {cityInfo.cityName}</button>
+      </form>
+    </div>
+  </div>
+);
+
+export default CitiesItem;
--- client/src/js/components/CitiesList.js
+++ client/src/js/components/CitiesList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SVGDelete from './SVGDelete';
+import CitiesItem from './CitiesItem';
 
 const CitiesList = ({
   cities,
@@ -7,26 +7,7 @@ const CitiesList = ({
   <section className="cities-list">
     <ul>
       {cities.map((city, index) => (
-        <li key={index}>
-          <div className="city-item">
-            <a href="#"><h2 className="city-list-title">{city.cityName}</h2></a>
-            <div className="city-info">
-              <p className="city-list-brew-count">{city.completedCount}/{city.brewTotal}</p>
-              <form className="city-list-delete-form" action="index.html" method="post">
-                <button type="submit" name="button"><SVGDelete /><span className="visually-hidden">Delete {city.cityName}</span></button>
-              </form>
-            </div>
-          </div>
-
-          <div className="delete-city-warning">
-            <h3>Delete {city.cityName}?</h3>
-            <p>Deleting a location will permanently remove it from your checklist and you will lose your visited brewery record.</p>
-            <form className="delete-city-form" action="index.html" method="post">
-              <button className="keep-city-btn" type="button" name="button">No, Keep {city.cityName}</button> {' '}
-              <button className="delete-city-btn" type="button" name="button"><SVGDelete />&nbsp;Yes, Delete {city.cityName}</button>
-            </form>
-          </div>
-        </li>
+        <li key={index}><CitiesItem cityInfo={city} /></li>
       ))}
     </ul>
   </section>
